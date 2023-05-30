8fa7895947dcc871c1305a9f947c36963a647687
dhruv221 <62287269+dhruv221@users.noreply.github.com>
2023-05-31 05:20:42 +0530

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,1 +1,26 @@
+#include "queue.h"
 
+queue queue_1(5);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(queue_1.isEmpty());
+  Serial.println(queue_1.isFull());
+  queue_1.enqueue(1);
+  queue_1.enqueue(2);
+  queue_1.enqueue(3);
+  queue_1.enqueue(4);
+  queue_1.enqueue(5);
+  Serial.println(queue_1.isEmpty());
+  Serial.println(queue_1.isFull());
+  for(int i = 0; i<5; i++) {
+    Serial.print(queue_1.dequeue());
+    Serial.print(", ");
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
